27fd6970a88aa4319c3dd616e7681f46f2110cea
Soprana Luca <37413900@telecomitalia.local>
2018-05-28 14:52:41 +0200

    change

---
+++ DK/dk_location.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+dk.py: 
+observe vehicle attribute (position) changes.
+"""
+from __future__ import print_function
+from dronekit import connect, VehicleMode
+import time
+
+#Set up option parsing to get connection string
+import argparse  
+parser = argparse.ArgumentParser(description='Print out vehicle state information. Connects to SITL on local PC by default.')
+parser.add_argument('--connect', 
+                   help="vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+connection_string = args.connect
+sitl = None
+
+
+#Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+
+# Connect to the Vehicle. 
+#   Set `wait_ready=True` to ensure default attributes are populated before `connect()` returns.
+print("\nConnecting to vehicle on: %s" % connection_string)
+vehicle = connect(connection_string, wait_ready=True)
+
+vehicle.wait_ready('autopilot_version')
+
+print(" Global Location: %s" % vehicle.location.global_frame)
+print(" Global Location (relative altitude): %s" % vehicle.location.global_relative_frame)
+print(" Local Location: %s" % vehicle.location.local_frame)
+print(" Attitude: %s" % vehicle.attitude)
+print(" Velocity: %s" % vehicle.velocity)
+print(" GPS: %s" % vehicle.gps_0)
+
